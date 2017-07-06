b4b32befc30ee9c5486a3933c159c62b36f4955a
Jerry Moxley <jmoxley@SJorgensenMac14.local>
2017-07-05 20:37:33 -0700

    generalized from NOAA script

--- Curl_GHRSST.R
+++ Curl_GHRSST.R
@@ -3,11 +3,11 @@
 library(RCurl)
 
 #### change
-setwd("~/Desktop/junk")
-dates<-seq(as.Date("2016/01/01"), as.Date("2016/02/01"), by = "week",format="%Y/%mm/%dd")
+setwd("/Users/jmoxley/Documents/MBA_GWS/RemoteSensing/test_download")
+dates<-seq(as.Date("2014/08/11"), as.Date("2014/08/17"), by = "week",format="%Y/%mm/%dd")
 minLat="35"
-maxLat="37"
-minLon="-122"
+maxLat="39"
+minLon="-125"
 maxLon="-120"
 ####
 
@@ -16,7 +16,7 @@ i<-1
 while (i < length(dates)){
   startdate<-dates[i]
   enddate<-dates[i+1]
-  filenm<-paste("jplG1SST_",startdate,".nc",sep="")
+  filenm<-paste("jplG1SST_mbay_",startdate,".nc",sep="")
   url<-paste0("http://coastwatch.pfeg.noaa.gov/erddap/griddap/jplG1SST.nc?SST[(",startdate,"):1:(",enddate,")][(",minLat,"):1:(",maxLat,")][(",minLon,"):1:(",maxLon,")]")
   print(startdate)
   f = CFILE(filenm,mode="wb")
